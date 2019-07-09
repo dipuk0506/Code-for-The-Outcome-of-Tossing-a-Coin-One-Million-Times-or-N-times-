907e87e167c06944e8fe84d3f94bcf697ba7feb9
H M Dipu Kabir <45284799+dipuk0506@users.noreply.github.com>
2019-07-09 20:27:20 +1000

    Update UQ_coins.m

--- UQ_coins.m
+++ UQ_coins.m
@@ -4,16 +4,15 @@
 % While computing the odds of tossing a coin for more than a 
 % thousand time there is a good chance of variable overflow with 
 % the traditional combination-based head counting. Therefore, we 
-% determine the cumulative probability with convolution and 
-% normalization.
+% determine the probability over iterations.
 
 clc;clear all
 Number_of_coins=1e6 %Can cnahge the number for a different N 
 y=[1 1]/2;
 for i=1:Number_of_coins-1
-    y=conv([1 1],y)/2;
+    y=[y 0]+[0 y];
+    y=y/2;
 end
-cy=cumsum(y) %CDF
+cy=cumsum(y)
 
 %save('CDF_Million_Toss','cy')
-
